958cbf79c05f31ffd8d942f5eca940af983781ef
febriyantiazahra <febriyantiazhraa@gmail.com>
2022-11-13 21:02:08 +0700

    membuat project springboot id card

---
+++ src/main/java/tugas_pws/pws_126/Pws126Application.java
@@ -0,0 +1,13 @@
+package tugas_pws.pws_126;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pws126Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pws126Application.class, args);
+	}
+
+}
---
+++ src/test/java/tugas_pws/pws_126/Pws126ApplicationTests.java
@@ -0,0 +1,13 @@
+package tugas_pws.pws_126;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pws126ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
